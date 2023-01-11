66b734bb2513f472c56470b38cd52322f1bf77c0
Tyler Zenisek <tzenisekj@gmail.com>
2023-01-11 09:58:46 -0600

    implemented base routing system

--- ZenChatWeb/src/App.tsx
+++ ZenChatWeb/src/App.tsx
@@ -1,11 +1,25 @@
-
+import { Routes, Route } from 'react-router-dom'; 
+import Login from './Pages/login' 
+import Registration from './Pages/registration'; 
+import Dashboard from './Pages/dashboard'; 
+import ErrorPage from './Pages/errorPage'; 
 
 function App() {
 
   return (
-    <div className="App">
-      <h1>ZenChat Home</h1>
-    </div>
+    <Routes>
+      <Route path="/" >
+        {/*Open routes*/}
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Registration />} />
+
+        {/*Protected routes*/}
+        <Route path="/:id" element={<Dashboard />} />
+
+        {/*Catch all route 404*/}
+        <Route path="*" element={<ErrorPage />} />
+      </Route>
+    </Routes>
   )
 }
 
---
+++ ZenChatWeb/src/Pages/errorPage.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+
+export default function errorPage() {
+  return (
+    <div>errorPage</div>
+  )
+}
+
